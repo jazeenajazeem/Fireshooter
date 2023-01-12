71b657a97c5af4567c12c777e1b6864118f215ec
Sakthy1 <114680260+Sakthy1@users.noreply.github.com>
2023-01-12 16:22:16 +0000

    New Projectile created
    upgraded the projectile with double power.

--- Player.java
+++ Player.java
@@ -14,8 +14,18 @@ public class Player extends Actor
      */
     int speed = 3;
     int time = 0;
+    WeaponButton weaponButton;
     public Player()
     {
+     setImage(new GreenfootImage(70,50));
+     getImage().setColor(Color.RED);
+     getImage().fillOval(0, 0, 50, 50);
+     getImage().setColor(Color.BLACK);
+     getImage().fillRect(50,20,70,10);
+    }
+    public Player(WeaponButton weaponButton)
+    {
+     this.weaponButton=weaponButton;
      setImage(new GreenfootImage(70,50));
      getImage().setColor(Color.RED);
      getImage().fillOval(0, 0, 50, 50);
@@ -46,14 +56,40 @@ public class Player extends Actor
         if(Greenfoot.isKeyDown("d"))
         setLocation(getX()+speed,getY());   
     }
+    
     public void fireProjectile()
     {
-        if(Greenfoot.mousePressed(null)){
+        if(Greenfoot.mousePressed(null) && weaponButton.weaponUpgrade == 1){
            Projectile projectile = new Projectile();
            getWorld().addObject(projectile, getX(),getY());
            projectile.setRotation(getRotation());
-           projectile.move(20);
+           projectile.move(25);
         }
+        if(Greenfoot.mousePressed(null) && weaponButton.weaponUpgrade == 2){
+           Projectile projectile = new Projectile();
+           getWorld().addObject(projectile, getX(),getY());
+           projectile.setRotation(getRotation() -5);
+           projectile.move(20);
+           Projectile projectile2 = new Projectile();
+           getWorld().addObject(projectile2, getX(),getY());
+           projectile2.setRotation(getRotation() + 5);
+           projectile2.move(5);
+        }
+        if(Greenfoot.mousePressed(null) && weaponButton.weaponUpgrade== 3){
+           Projectile projectile = new Projectile();
+           getWorld().addObject(projectile, getX(),getY());
+           projectile.setRotation(getRotation() -5);
+           projectile.move(20);
+           Projectile projectile2 = new Projectile();
+           getWorld().addObject(projectile2, getX(),getY());
+           projectile2.setRotation(getRotation() +5);
+           projectile2.move(5);
+           Projectile projectile3 = new Projectile();
+           getWorld().addObject(projectile3, getX(),getY());
+           projectile3.setRotation(getRotation());
+           projectile3.move(5);
+        }
+        
     }
     public boolean hitByZombie()
     {
--- WeaponButton.java
+++ WeaponButton.java
@@ -13,16 +13,18 @@ public class WeaponButton extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     Counter counter;
+    int weaponUpgrade= 1;
     public WeaponButton(Counter counter)
     {
-     this.counter =counter;
-     setImage(new GreenfootImage("Weapon  \n Upgrade",25, Color.BLACK,new Color(0,0,0,0)));
+       this.counter =counter;
+       setImage(new GreenfootImage("Weapon  \n Upgrade",25, Color.BLACK,new Color(0,0,0,0)));
     }
     public void act()
-    {
-   if(Greenfoot.mousePressed(this))
+    { 
+        if(Greenfoot.mousePressed(this) && counter.money>149)
         {
-            counter.money=counter.money-150;
-    }
+           counter.money-=-150;
+           weaponUpgrade++;
+        }
 }
 }
