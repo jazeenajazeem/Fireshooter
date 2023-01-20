feed8bea48a0f6992e5b027f4edaa037e1ca35c6
Sakthy1 <114680260+Sakthy1@users.noreply.github.com>
2023-01-20 13:07:36 +0000

    updates
    updates

--- WeaponButton.java
+++ WeaponButton.java
@@ -6,18 +6,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number 4 or jan)
  */
+//weapon button sub class is created and is inherited from Actor class
 public class WeaponButton extends Actor
 {
     /**
      * Act - do whatever the Weapon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    //weapon upgraded and add color
+    //this is the weaponButton is  created to upgrade the super power  and add the color.
+    //counter creation.
+    //storing the counter 
     Counter counter;
     int weaponUpgrade= 1;
+    //This is the WeaponButton method 
     public WeaponButton(Counter counter)
     {
        this.counter =counter;
+       //weaponButtonImage is setting ...
        setImage(new GreenfootImage("Weapon  \n Upgrade",25, Color.BLACK,new Color(0,0,0,0)));
     }
     public void act()
