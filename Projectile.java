f0dc80ea3edd6ae9584cfaf16fb44463a2bdcb7d
jazeenajazeem <116195400+jazeenajazeem@users.noreply.github.com>
2023-01-23 09:16:46 +0000

    updated
    updated

--- Projectile.java
+++ Projectile.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-// Create Projectile class
+// Create Projectile sub class
 public class Projectile extends Actor
 {
     
